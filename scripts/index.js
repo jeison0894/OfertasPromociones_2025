e7434bba495b99aca35ce33cf1883e150b1be3c0
Jeison Garzon <jeisson0894@gmail.com>
2025-07-01 00:07:49 -0500

    untrack script folder

--- scripts/index.js
+++
@@ -1,4 +0,0 @@
-import './mainScriptGoogleSheet.js';
-import './categoryMenu.js';
-import './iframeHeightManager.js';
-import './toggleSeoButton.js';
--- scripts/toggleSeoButton.js
+++
@@ -1,5 +0,0 @@
-let seoButton=document.querySelector('.seo__toggle-btn')
-seoButton.addEventListener('click',()=>{let seoContent=document.getElementById('seoContent')
-if(seoContent.style.display==='none'||seoContent.style.display===''){seoContent.style.display='block'
-seoButton.textContent='Ver menos'}else{seoContent.style.display='none'
-seoButton.textContent='Ver más'}})
